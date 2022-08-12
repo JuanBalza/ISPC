f2ce29f2ef16f63cf1c9a15993fd6219642e3c91
romeomoreno <104795749+romeomoreno@users.noreply.github.com>
2022-08-12 15:36:44 -0300

    Trabajo Final

---
+++ trabajofinal.py
@@ -0,0 +1,11 @@
+from operator import and_
+import os
+os.system('cls')
+
+
+Dueno2 = [23546987,  'Maria',  'Perez', 4789689,  'Pueyrredon  811']
+for i in Dueno2:
+    if i != Dueno2[0] and i != Dueno2[3]: 
+        print(i)
+        
+    
