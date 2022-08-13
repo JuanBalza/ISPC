ac587a2dec3b27976ca4442cf0e158f3485fa0cd
JuanBalza <83681810+JuanBalza@users.noreply.github.com>
2022-08-13 12:28:56 -0300

    Add files via upload
    actualizacion de tabla base de datos punto 3

---
+++ Database,peluqueriacanina punto3.sql
@@ -0,0 +1,58 @@
+CREATE DATABASE Peluqueriacanina;
+
+use Peluqueriacanina;
+
+CREATE TABLE Dueño
+(
+DNI int,
+Nombre varchar (25),
+Apellido varchar (25),
+Teléfono varchar(25),
+Dirección varchar (25),
+CONSTRAINT Id_DNI PRIMARY KEY (DNI)
+);
+
+CREATE TABLE Perro 
+(
+Id_Perro int,
+Nombre varchar (25),
+Fecha_nac date,
+Sexo varchar(25),
+DNI_Dueño int,
+CONSTRAINT id_Perro PRIMARY KEY (Id_Perro),
+CONSTRAINT f_dni FOREIGN KEY (DNI_Dueño) REFERENCES Dueño (DNI)
+);
+
+CREATE TABLE Historial
+(
+Id_Historial int,
+Fecha date,
+Perro int,
+Descripción text,
+Monto int,
+CONSTRAINT Id_historial PRIMARY KEY (Id_Historial),
+CONSTRAINT f_perro FOREIGN KEY (Perro) REFERENCES Perro (Id_Perro) 
+);
+
+use Peluqueriacanina;
+
+INSERT INTO Dueño (DNI, Nombre, Apellido, Teléfono, Dirección) VALUES 
+('42087410', 'Diego', 'Arias', '4531945', 'Las Palmas 85');
+
+INSERT INTO Perro (Id_Perro, Nombre, Fecha_nac, Sexo, DNI_Dueño) VALUES 
+('001', 'Rex', '00-12-16', 'Macho', '42087410');
+
+INSERT INTO Historial (Id_Historial, Fecha, Perro, Descripción, Monto) VALUES 
+('1', '02-08-24', '45', 'Obejero Aleman', '4500');
+
+INSERT INTO Dueño (DNI, Nombre, Apellido, Teléfono, Dirección) VALUES 
+('39087410', 'Luis', 'Perez', '4451945', 'Los Alamos 85');
+
+INSERT INTO Perro (Id_Perro, Nombre, Fecha_nac, Sexo, DNI_Dueño) VALUES 
+('002', 'Rex', '00-12-16', 'Macho', '39087410');
+
+INSERT INTO Historial (Id_Historial, Fecha, Perro, Descripción, Monto) VALUES 
+('2', '00-11-14', '002', 'Labrador', '5000');
+
+DELETE FROM Historial
+Where Perro = '002';
