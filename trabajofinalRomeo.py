005f2e9d38ec08b4e35cb5d64b227996be110431
romeomoreno <104795749+romeomoreno@users.noreply.github.com>
2022-08-13 10:24:37 -0300

    Trabajo Final

---
+++ trabajofinalRomeo.py
@@ -0,0 +1,16 @@
+from operator import and_
+import os
+os.system('cls')
+
+#  2)   Crear una lista en Python denominada “Dueno2”  y recorrerla con un bucle  mostrando sus elementos por pantalla con excepción del DNI y el apellido. Los elementos de la lista son:
+
+    #       23546987,  “Maria”,  “Perez”, 4789689,  “Pueyrredon  811”
+
+ #que representan los datos del dueño de un perro (DNI, nombre, apellido, teléfono y dirección) 
+
+Dueno2 = [23546987,  'Maria',  'Perez', 4789689,  'Pueyrredon  811']
+for i in Dueno2:
+    if i != Dueno2[0] and i != Dueno2[2]: 
+        print(i)
+        
+    
